1ea2e5ea3a2831d213b8465d9d711c7e01668ac1
wisheee <wishee.u@gmail.com>
2022-02-23 10:29:42 +0900

    feat: header scroll event 추가

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,23 @@
 (() => {
+  // elements
+  const header = document.querySelector('header');
+
+  function handleScrollHeader() {
+    if (window.pageYOffset < 1) {
+      header.classList.remove('moving');
+      return;
+    }
+
+    header.classList.add('moving');
+  }
+
+  window.addEventListener('scroll', e => {
+    handleScrollHeader();
+  });
+
   window.addEventListener('DOMContentLoaded', () => {
     document.body.classList.remove('before-load');
   });
+
+  handleScrollHeader();
 })();
